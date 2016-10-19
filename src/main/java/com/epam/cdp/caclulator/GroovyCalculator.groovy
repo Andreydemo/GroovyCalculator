cfbe2f9de8e0877fbc889deb8a139dea974b8b20
Andrii_Korkoshko <demosoft@bk.ru>
2016-10-19 10:59:22 +0300

    Initial commit

---
+++ src/main/java/com/epam/cdp/caclulator/GroovyCalculator.groovy
@@ -0,0 +1,12 @@
+package com.epam.cdp.caclulator
+
+/**
+ * Created by Andrii_Korkoshko on 10/18/2016.
+ */
+class GroovyCalculator implements Calculator {
+
+    @Override
+    double calculate(String statement) {
+        return 0
+    }
+}
